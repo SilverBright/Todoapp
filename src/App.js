404e77482a2cd5fc84bc5799bdcf8e27462146ae
github username <github email address>
2019-05-05 15:45:07 -0700

    add Materialize for css, delete App.css, App.test, and logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'go shopping'},
+      {id: 2, content: 'pick up dry cleaning'}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="center blue-text">My Todo List</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
-
+  
 export default App;
