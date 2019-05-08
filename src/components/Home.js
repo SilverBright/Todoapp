57091616a4bdc06ed46997fad454f774b6c1a01a
github username <github email address>
2019-05-07 18:55:02 -0700

    install redux and react-redux, create reducers folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,7 +31,7 @@ class Home extends Component {
                         <img src={Target} alt="target"/>
                         <div className="card-content">
                             <Link to={'/' + post.id}>
-                            <span className="card-title">{post.title}</span>
+                            <span className="card-title purple-text">{post.title}</span>
                             </Link>
                             <p>{post.body}</p>
                         </div>
