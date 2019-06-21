27a74d9f1b462a065cb8300229c441a5f897cb63
github username <barbara.m.washington@gmail.com>
2019-06-21 10:26:05 -0700

    update associated email address with repository

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,20 +8,20 @@ class Post extends Component {
     this.props.deletePost(this.props.post.id);
     this.props.history.push('/home');
   }
-  // state = {
-  //   post: null
-  // }
-  // componentDidMount(){
-  //   // console.log(this.props)
-  //   let id = this.props.match.params.post_id;
-  //   axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-  //     .then(res => {
-  //       this.setState({
-  //         post: res.data
-  //       })
-  //       // console.log(res)
-  //     })
-  // }
+  state = {
+    post: null
+  }
+  componentDidMount(){
+    // console.log(this.props)
+    let id = this.props.match.params.post_id;
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then(res => {
+        this.setState({
+          post: res.data
+        })
+        // console.log(res)
+      })
+  }
   render() {
     console.log(this.props)
     const post = this.props.post ? (
